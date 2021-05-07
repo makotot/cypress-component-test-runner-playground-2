be688a5a4a655ec6c5da67e7455d0f99e4371ac9
makotot <m.tateno@prog-8.com>
2021-05-07 17:59:00 +0900

    tweak

--- src/App.tsx
+++ src/App.tsx
@@ -14,51 +14,50 @@ function App() {
 
   return (
     <div className="App">
-      <div>
-        <Scrollspy sectionRefs={sectionRefs}>
-          {() => (
-            <div>
-              <ul
-                data-cy="nav-wrapper"
-                style={{
-                  listStyle: 'none',
-                  position: 'fixed',
-                  top: 0,
-                  right: 0,
-                  backgroundColor: '#fff',
-                  padding: '1rem',
-                }}>
-                {new Array(SIZE).fill(0).map((_, i) => (
-                  <li key={i}>
-                    <a href={`#section-${i}`} data-cy={`nav-item`}>
-                      section-{i}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-              <div data-cy="section-wrapper">
-                {new Array(SIZE).fill(0).map((_, i) => (
-                  <div
-                    id={`section-${i}`}
-                    data-cy={`section-item`}
-                    key={i}
-                    style={{
-                      display: 'flex',
-                      justifyContent: 'center',
-                      alignItems: 'center',
-                      height: '500px',
-                      backgroundColor: `#${i}${i}${i}`,
-                      color: '#fff',
-                      fontSize: '2rem',
-                    }}>
-                    {i}
-                  </div>
-                ))}
-              </div>
+      <Scrollspy sectionRefs={sectionRefs}>
+        {() => (
+          <div>
+            <ul
+              data-cy="nav-wrapper"
+              style={{
+                listStyle: 'none',
+                position: 'fixed',
+                top: 0,
+                right: 0,
+                backgroundColor: '#fff',
+                padding: '1rem',
+              }}>
+              {new Array(SIZE).fill(0).map((_, i) => (
+                <li key={i}>
+                  <a href={`#section-${i}`} data-cy={`nav-item`}>
+                    section-{i}
+                  </a>
+                </li>
+              ))}
+            </ul>
+            <div data-cy="section-wrapper">
+              {new Array(SIZE).fill(0).map((_, i) => (
+                <div
+                  id={`section-${i}`}
+                  data-cy={`section-item`}
+                  ref={sectionRefs[i]}
+                  key={i}
+                  style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    height: '500px',
+                    backgroundColor: `#${i}${i}${i}`,
+                    color: '#fff',
+                    fontSize: '2rem',
+                  }}>
+                  {i}
+                </div>
+              ))}
             </div>
-          )}
-        </Scrollspy>
-      </div>
+          </div>
+        )}
+      </Scrollspy>
     </div>
   );
 }
