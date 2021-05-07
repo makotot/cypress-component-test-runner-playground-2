73e06efa322b89511e50e23a7d11a4d30649c59a
makotot <m.tateno@prog-8.com>
2021-05-07 17:34:33 +0900

    red

---
+++ src/App.spec.tsx
@@ -0,0 +1,27 @@
+import { mount } from '@cypress/react';
+import App from './App';
+
+describe('App', () => {
+  beforeEach(() => {
+    mount(<App />);
+    cy.scrollTo(0, 0);
+  });
+
+  it('childrenがrenderされること', () => {
+    cy.get('[data-cy=nav-wrapper]').children().should('have.length', 5);
+    cy.get('[data-cy=section-wrapper]').children().should('have.length', 5);
+  });
+  it('viewport内の要素に対応するナビゲーションがアクティブになること', () => {
+    cy.get('[data-cy=section-item]').eq(0).should('have.class', 'active');
+    cy.get('[data-cy=nav-item]').eq(0).should('have.class', 'active');
+  });
+  it('viewport内に収まる要素が変わったら、対応するナビゲーションがアクティブな状態になること', () => {
+    cy.get('[data-cy=section-item]').eq(0).should('have.class', 'active');
+    cy.get('[data-cy=nav-item]').eq(0).should('have.class', 'active');
+
+    cy.get('[data-cy=section-item').eq(1).scrollIntoView();
+
+    cy.get('[data-cy=section-item]').eq(1).should('have.class', 'active');
+    cy.get('[data-cy=nav-item]').eq(1).should('have.class', 'active');
+  });
+});
--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,63 @@
-import React, { useEffect, useRef } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useRef } from "react";
+import { Scrollspy } from './Scrollspy'
+
+const SIZE = 5;
 
 function App() {
-  const target = useRef(null)
-
-  useEffect(() => {
-
-  }, []);
+  const sectionRefs = [
+    useRef<HTMLDivElement>(null),
+    useRef<HTMLDivElement>(null),
+    useRef<HTMLDivElement>(null),
+    useRef<HTMLDivElement>(null),
+    useRef<HTMLDivElement>(null),
+  ];
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <div style={{height: '100vh'}} ref={target}>
+      <div>
+        <Scrollspy sectionRefs={sectionRefs}>
+          {() => (
+            <div>
+              <ul
+                data-cy="nav-wrapper"
+                style={{
+                  listStyle: 'none',
+                  position: 'fixed',
+                  top: 0,
+                  right: 0,
+                  backgroundColor: '#fff',
+                  padding: '1rem',
+                }}>
+                {new Array(SIZE).fill(0).map((_, i) => (
+                  <li key={i}>
+                    <a href={`#section-${i}`} data-cy={`nav-item`}>
+                      section-{i}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+              <div data-cy="section-wrapper">
+                {new Array(SIZE).fill(0).map((_, i) => (
+                  <div
+                    id={`section-${i}`}
+                    data-cy={`section-item`}
+                    key={i}
+                    style={{
+                      display: 'flex',
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                      height: '500px',
+                      backgroundColor: `#${i}${i}${i}`,
+                      color: '#fff',
+                      fontSize: '2rem',
+                    }}>
+                    {i}
+                  </div>
+                ))}
+              </div>
+            </div>
+          )}
+        </Scrollspy>
       </div>
     </div>
   );
